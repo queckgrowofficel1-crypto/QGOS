0a617941ce33bbd6cb47478b61bcde667a447be7
queckgrowofficel1-crypto <queckgrowofficel1@gmail.com>
2026-08-18 17:38:58 +0530

    feat: wire core modules into application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { GenealogyModule } from './genealogy/genealogy.module';
 import { HealthController } from './health/health.controller';
+import { PackagesModule } from './packages/packages.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    PackagesModule,
+    GenealogyModule,
+  ],
   controllers: [HealthController],
 })
 export class AppModule {}
