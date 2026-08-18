0cb06b717abacf1199e96a29a389b4a35ea06788
queckgrowofficel1-crypto <queckgrowofficel1@gmail.com>
2026-08-18 14:52:54 +0530

    feat(phase4): wire scheduler into automation module

--- src/modules/automation/automation.module.ts
+++ src/modules/automation/automation.module.ts
@@ -3,10 +3,11 @@ import { AutomationController } from './automation.controller';
 import { AutomationService } from './automation.service';
 import { AutomationStore } from './automation.store';
 import { AutomationExecutor } from './automation.executor';
+import { AutomationScheduler } from './automation.scheduler';
 
 @Module({
   controllers: [AutomationController],
-  providers: [AutomationService, AutomationStore, AutomationExecutor],
-  exports: [AutomationService, AutomationExecutor],
+  providers: [AutomationService, AutomationStore, AutomationExecutor, AutomationScheduler],
+  exports: [AutomationService, AutomationExecutor, AutomationScheduler],
 })
 export class AutomationModule {}
