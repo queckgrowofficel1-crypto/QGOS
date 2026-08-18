74b6bdcb7e30edb82dd2f872cded1f3756258e5a
queckgrowofficel1-crypto <queckgrowofficel1@gmail.com>
2026-08-18 13:24:42 +0530

    feat(phase4): wire automation state store

--- src/modules/automation/automation.module.ts
+++ src/modules/automation/automation.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AutomationController } from './automation.controller';
 import { AutomationService } from './automation.service';
+import { AutomationStore } from './automation.store';
 
 @Module({
   controllers: [AutomationController],
-  providers: [AutomationService],
+  providers: [AutomationService, AutomationStore],
   exports: [AutomationService],
 })
 export class AutomationModule {}
