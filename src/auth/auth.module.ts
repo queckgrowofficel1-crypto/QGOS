f426faf5b365bfbb845c5abf96c4d199b127d282
queckgrowofficel1-crypto <queckgrowofficel1@gmail.com>
2026-08-18 17:34:54 +0530

    feat: add auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+@Module({
+  imports: [JwtModule.register({ secret: process.env.JWT_SECRET ?? 'qgos-development-secret' })],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
+export class AuthModule {}
