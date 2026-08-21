1562e4009c6cc2e769e9815fed407b0bb00e9ff1
queckgrowofficel1-crypto <queckgrowofficel1@gmail.com>
2026-08-21 13:51:35 +0530

    fix(lint): remove unused IsObject import

--- src/modules/ai/workflow.controller.ts
+++ src/modules/ai/workflow.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { IsEnum, IsNotEmpty, IsObject, IsOptional, IsString, MaxLength } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 import { WorkflowTriggerType } from '@prisma/client';
 import { WorkflowService } from './workflow.service';
 
