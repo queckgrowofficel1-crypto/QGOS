b5e468f30c475661d6d2cb4f0b67ff191c821209
queckgrowofficel1-crypto <queckgrowofficel1@gmail.com>
2026-08-24 12:32:40 +0530

    feat(phase5): register income engine service

--- src/modules/financial/financial.module.ts
+++ src/modules/financial/financial.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { FinancialController } from './financial.controller';
 import { FinancialService } from './financial.service';
+import { IncomeService } from './income.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [FinancialController],
-  providers: [FinancialService],
-  exports: [FinancialService],
+  providers: [FinancialService, IncomeService],
+  exports: [FinancialService, IncomeService],
 })
 export class FinancialModule {}
